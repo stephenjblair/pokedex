7293fd527964595d4be7eb218f25cd8ecf80d414
Stephen Blair <stephenjohnblair@outlook.com>
2020-04-19 12:50:44 +0100

    Updated styles and README file

--- README.md
+++ README.md
@@ -66,3 +66,5 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `yarn build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+## Hit refresh to start the game again!
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -16,11 +16,16 @@
          ]
      };
      render() {
+         let title;
+         if(this.props.isWinner) {
+             title = <h2 className="Pokedex-winner">Winning Hand!</h2>;
+         } else {
+             title = <h2 className="Pokedex-loser"> Losing Hand!</h2> 
+         }
          return (
              <div className="Pokedex">
-                 <h1>Pokedex!</h1>
+                 {title}
                  <p>Total Experience: {this.props.exp}</p>
-                 <p>{this.props.isWinner ? "Winner!" : "Loser!"}</p>
                  <div className="Pokedex-cards">
                  {this.props.pokemon.map((p) => (
                      <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
