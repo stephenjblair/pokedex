0fd431784e2ab0773cca0383a2857cba0a8710db
Stephen Blair <stephenjohnblair@outlook.com>
2020-04-19 10:32:16 +0100

    Added Pokecard component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Pokecard from './components/Pokecard';
 
 
 function App() {
   return (
     <div className="App">
-      <h1> TEST</h1>
+      <Pokecard id={4} name="Charmander" type="fire" exp={62} />
     </div>
   );
 }
